d9e13688629891a232bc44a6e8fa2f6b83ad9427
Brilock <artem_zezyulin@mail.ru>
2022-04-19 11:14:26 +0300

    Fixed Role and User

--- src/main/java/com/example/testservice/entity/Role.java
+++ src/main/java/com/example/testservice/entity/Role.java
@@ -1,2 +1,31 @@
-package com.example.testservice.entity;public class Role {
+package com.example.testservice.entity;
+
+import org.hibernate.annotations.Table;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(appliesTo = "t_role")
+public class Role implements GrantedAuthority {
+    @Id
+    private Long id;
+    private String name;
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+
+    }
+
+    public Role(Long id) {
+
+    }
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
 }
--- src/main/java/com/example/testservice/entity/User.java
+++ src/main/java/com/example/testservice/entity/User.java
@@ -1,2 +1,97 @@
-package com.example.testservice.entity;public class User {
+package com.example.testservice.entity;
+
+import org.hibernate.annotations.Table; //именно hibernate переводит объекты в данные таблиц БД (ORM)
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity //поля класса имеют отображение в БД
+@Table(appliesTo = "t_user") //с какой именно таблицей
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //генерацией id будет заниматься БД
+    private long id;
+    @Size(min=2, message = "не меньше 5 знаков")
+    private String username;
+    @Size(min=2, message = "не меньше 5 знаков")
+    private String password;
+    @Transient
+    private String passwordConfirm;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public User() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
